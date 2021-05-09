7150cb276063e8b4c77f5ac1e1d71367a15afb09
htsign <htsign@outlook.com>
2021-05-10 02:17:18 +0900

    add some tests

--- test/polyfills.test.js
+++ test/polyfills.test.js
@@ -57,22 +57,36 @@ describe('polyfills.js', () => {
       expect([1, 3, 6, 7].some(x => x % 2 === 0)).toBeTruthy();
     });
     test('Array.prototype.reduce', () => {
+      expect([2, 4, 6, 8].reduce((acc, cur, i) => i % 2 === 0 ? acc - cur : acc + cur, 100)).toEqual(104);
       expect([[0, 1], [2, 3], [4, 5]].reduce((acc, cur) => acc.concat(cur))).toEqual([0, 1, 2, 3, 4, 5]);
+    });
+    test('Array.prototype.reduceRight', () => {
+      expect([2, 4, 6, 8].reduceRight((acc, cur, i) => i % 2 === 0 ? acc - cur : acc + cur, 100)).toEqual(104);
+      expect([[0, 1], [2, 3], [4, 5]].reduceRight((acc, cur) => acc.concat(cur))).toEqual([4, 5, 2, 3, 0, 1]);
     });
     test('Array.prototype.find', () => {
       expect(['abc', 'def', 'ghi'].find(s => s.includes('e'))).toEqual('def');
     });
     test('Array.prototype.indexOf', () => {
       expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3)).toEqual(2);
+      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3, 3)).toEqual(7);
+      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3, 8)).toEqual(-1);
     });
     test('Array.prototype.includes', () => {
       expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3)).toBeTruthy();
+      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3, 3)).toBeTruthy();
+      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3, 8)).toBeFalsy();
     });
     test('Array.prototype.flat', () => {
       const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
       expect(arr.flat()).toEqual(arr.flat(1));
       expect(arr.flat(1)).toEqual([1, 2, 3, 4, [5, 6, [7, 8, [9, 10]]]]);
       expect(arr.flat(2)).toEqual([1, 2, 3, 4, 5, 6, [7, 8, [9, 10]]]);
+      expect(arr.flat(Infinity)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      expect(arr.flat(0)).toEqual(arr);
+    });
+    test('Array.prototype.flatMap', () => {
+      expect([1, 2, 3, 4].flatMap(x => [x, x * 2])).toEqual([1, 2, 2, 4, 3, 6, 4, 8]);
     });
   });
 });
