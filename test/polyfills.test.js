f1087de98256ad99740be26ee755ebb5a50a9536
htsign <htsign@outlook.com>
2026-02-28 15:06:31 +0900

    use node TestRunner instead of jest

--- test/polyfills.test.js
+++ test/polyfills.test.js
@@ -1,93 +1,95 @@
 require('../js/polyfills');
 
-describe('polyfills.js', () => {
-  describe('Object static members', () => {
+const { suite, test } = require('node:test');
+const assert = require('node:assert/strict');
+
+suite('polyfills.js', () => {
+  suite('Object static members', () => {
     const o = { a: 1, b: 2 };
     test('Object.keys', () => {
-      expect(Object.keys(o)).toEqual(['a', 'b']);
+      assert.deepStrictEqual(Object.keys(o), ['a', 'b']);
     });
     test('Object.values', () => {
-      expect(Object.values(o)).toEqual([1, 2]);
+      assert.deepStrictEqual(Object.values(o), [1, 2]);
     });
     test('Object.entries', () => {
-      expect(Object.entries(o)).toEqual([['a', 1], ['b', 2]]);
+      assert.deepStrictEqual(Object.entries(o), [['a', 1], ['b', 2]]);
     });
     test('Object.fromEntries', () => {
-      expect(Object.fromEntries([['a', 1], ['b', 2]])).toEqual(o);
+      assert.deepStrictEqual(Object.fromEntries([['a', 1], ['b', 2]]), o);
     });
 
     const o2 = { ...o };
     const exp = { a: 1, b: 2, c: 3 };
     test('Object.assign', () => {
-      expect(Object.assign(o2, { c: 3 })).toEqual(exp);
+      assert.deepStrictEqual(Object.assign(o2, { c: 3 }), exp);
     });
     test('Object.assign has side effect', () => {
-      expect(o2).toEqual(exp);
+      assert.deepStrictEqual(o2, exp);
     });
   });
 
-  describe('Function prototype members', () => {
+  suite('Function prototype members', () => {
     test('Function.prototype.bind', () => {
-      expect((function () { return this }).bind({ a: 1 })()).toEqual({ a: 1 });
+      assert.deepStrictEqual((function () { return this }).bind({ a: 1 })(), { a: 1 });
 
       const f = function () { return Array.prototype.slice.call(arguments) };
       const g = f.bind(undefined, 1, 2);
-      expect(g(3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
+      assert.deepStrictEqual(g(3, 4, 5), [1, 2, 3, 4, 5]);
     });
   });
 
-  describe('Array prototype members', () => {
+  suite('Array prototype members', () => {
     test('Array.prototype.map', () => {
-      expect([1, 2, 3].map(x => x + 1)).toEqual([2, 3, 4]);
+      assert.deepStrictEqual([1, 2, 3].map(x => x + 1), [2, 3, 4]);
     });
     test('Array.prototype.filter', () => {
-      expect([1, 2, 3, 4, 5].filter(x => x % 2)).toEqual([1, 3, 5]);
+      assert.deepStrictEqual([1, 2, 3, 4, 5].filter(x => x % 2), [1, 3, 5]);
     });
     test('Array.prototype.forEach', () => {
       const exp = [];
       [1, 2, 3].forEach(x => exp.push(x));
-      expect(exp).toEqual([1, 2, 3]);
+      assert.deepStrictEqual(exp, [1, 2, 3]);
     });
     test('Array.prototype.every', () => {
-      expect([2, 4, 6, 8].every(x => x % 2 === 0)).toBeTruthy();
-      expect([2, 4, 5, 8].every(x => x & 2 === 0)).toBeFalsy();
+      assert.ok([2, 4, 6, 8].every(x => x % 2 === 0));
+      assert.strictEqual([2, 4, 5, 8].every(x => x & 2 === 0), false);
     });
     test('Array.prototype.some', () => {
-      expect([1, 3, 5, 7].some(x => x % 2 === 0)).toBeFalsy();
-      expect([1, 3, 6, 7].some(x => x % 2 === 0)).toBeTruthy();
+      assert.strictEqual([1, 3, 5, 7].some(x => x % 2 === 0), false);
+      assert.ok([1, 3, 6, 7].some(x => x % 2 === 0));
     });
     test('Array.prototype.reduce', () => {
-      expect([2, 4, 6, 8].reduce((acc, cur, i) => i % 2 === 0 ? acc - cur : acc + cur, 100)).toEqual(104);
-      expect([[0, 1], [2, 3], [4, 5]].reduce((acc, cur) => acc.concat(cur))).toEqual([0, 1, 2, 3, 4, 5]);
+      assert.deepStrictEqual([2, 4, 6, 8].reduce((acc, cur, i) => i % 2 === 0 ? acc - cur : acc + cur, 100), 104);
+      assert.deepStrictEqual([[0, 1], [2, 3], [4, 5]].reduce((acc, cur) => acc.concat(cur)), [0, 1, 2, 3, 4, 5]);
     });
     test('Array.prototype.reduceRight', () => {
-      expect([2, 4, 6, 8].reduceRight((acc, cur, i) => i % 2 === 0 ? acc - cur : acc + cur, 100)).toEqual(104);
-      expect([[0, 1], [2, 3], [4, 5]].reduceRight((acc, cur) => acc.concat(cur))).toEqual([4, 5, 2, 3, 0, 1]);
+      assert.deepStrictEqual([2, 4, 6, 8].reduceRight((acc, cur, i) => i % 2 === 0 ? acc - cur : acc + cur, 100), 104);
+      assert.deepStrictEqual([[0, 1], [2, 3], [4, 5]].reduceRight((acc, cur) => acc.concat(cur)), [4, 5, 2, 3, 0, 1]);
     });
     test('Array.prototype.find', () => {
-      expect(['abc', 'def', 'ghi'].find(s => s.includes('e'))).toEqual('def');
+      assert.strictEqual(['abc', 'def', 'ghi'].find(s => s.includes('e')), 'def');
     });
     test('Array.prototype.indexOf', () => {
-      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3)).toEqual(2);
-      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3, 3)).toEqual(7);
-      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3, 8)).toEqual(-1);
+      assert.deepStrictEqual([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3), 2);
+      assert.deepStrictEqual([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3, 3), 7);
+      assert.deepStrictEqual([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].indexOf(3, 8), -1);
     });
     test('Array.prototype.includes', () => {
-      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3)).toBeTruthy();
-      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3, 3)).toBeTruthy();
-      expect([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3, 8)).toBeFalsy();
+      assert.ok([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3));
+      assert.ok([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3, 3));
+      assert.strictEqual([1, 2, 3, 4, 5, 1, 2, 3, 4, 5].includes(3, 8), false);
     });
     test('Array.prototype.flat', () => {
       const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
-      expect(arr.flat()).toEqual(arr.flat(1));
-      expect(arr.flat(1)).toEqual([1, 2, 3, 4, [5, 6, [7, 8, [9, 10]]]]);
-      expect(arr.flat(2)).toEqual([1, 2, 3, 4, 5, 6, [7, 8, [9, 10]]]);
-      expect(arr.flat(Infinity)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-      expect(arr.flat(0)).toEqual(arr);
+      assert.deepStrictEqual(arr.flat(), arr.flat(1));
+      assert.deepStrictEqual(arr.flat(1), [1, 2, 3, 4, [5, 6, [7, 8, [9, 10]]]]);
+      assert.deepStrictEqual(arr.flat(2), [1, 2, 3, 4, 5, 6, [7, 8, [9, 10]]]);
+      assert.deepStrictEqual(arr.flat(Infinity), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      assert.deepStrictEqual(arr.flat(0), arr);
     });
     test('Array.prototype.flatMap', () => {
-      expect([1, 2, 3, 4].flatMap(x => [x, x * 2])).toEqual([1, 2, 2, 4, 3, 6, 4, 8]);
+      assert.deepStrictEqual([1, 2, 3, 4].flatMap(x => [x, x * 2]), [1, 2, 2, 4, 3, 6, 4, 8]);
     });
   });
 });
-
